6dba141a462e190617a162bfc265a464d4ed8791
tonmoy <tonmoy3673@gmail.com>
2024-06-13 00:17:30 +0600

    :bulb: create private route

---
+++ src/Components/Router/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../AuthContext/AuthProvider";
+
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return (
+      <div className="d-flex mx-auto py-3 text-center">
+        <p className="me-2">Loading...</p>{" "}
+        <progress className="progress w-56 mt-6 md:mt-10"></progress>
+      </div>
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate state={{ from: location }} to="/login" replace />;
+};
+
+export default PrivateRoute;
