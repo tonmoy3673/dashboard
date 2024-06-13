35d6570f6e5524778f8be1c5ffcfa45870b3cb8f
tonmoy <tonmoy3673@gmail.com>
2024-06-13 17:22:01 +0600

    :tada: display book details

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -3,6 +3,7 @@ import ErrorPage from "../ErrorPage/ErrorPage";
 import Home from "../HomePage/Home";
 
 import MainLayout from "../Layout/MainLayout";
+import BookDetails from "../Pages/Books/BookDetails";
 import Books from "../Pages/Books/Books";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
@@ -30,7 +31,12 @@ export  const router=createBrowserRouter([
                 loader:()=>fetch('http://localhost:5000/books')
 
             },
-            
+            {
+                path:'/books/:id',
+                element:<BookDetails/>,
+                loader:({params})=>fetch(`http://localhost:5000/books/${params.id}`)
+            },
+
             {
                 path:'/login',
                 element:<Login/>
